38905ad08d8f6ba2c217249addb0a1e399d6d644
anurag <anuragpathakoffl@gmail.com>
2023-10-18 23:12:14 +0530

    added abput component

--- umedy/src/App.jsx
+++ umedy/src/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
+import Courses from "./components/Courses";
+
 const App = () => {
   return (
     <div>
@@ -8,6 +11,12 @@ const App = () => {
       <main>
         <div id="home">
           <Home />
+        </div>
+        <div id="about">
+          <About />
+        </div>
+        <div id="courses">
+          <Courses />
         </div>
       </main>
     </div>
